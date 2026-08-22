9c6fffe1cb04a489b52215ffd8020882d6d54b07
Bruno Neckel <neckel.bw@gmail.com>
2026-08-22 19:53:36 -0300

    feat: add registro screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
   },
+  {
+    path: 'registro',
+    loadComponent: () => import('./features/auth/registro/registro').then((m) => m.Registro),
+  },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', redirectTo: 'login' },
 ];
