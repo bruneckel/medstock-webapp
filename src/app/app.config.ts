16e513f60f04d532a3adec8683ad1cb8cd7eb859
Bruno Neckel <neckel.bw@gmail.com>
2026-08-22 19:28:27 -0300

    feat: provide HttpClient with interceptor support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -7,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([])),
     provideRouter(routes)
   ]
 };
