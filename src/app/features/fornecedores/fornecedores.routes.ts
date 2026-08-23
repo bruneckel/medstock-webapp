70ac5e6b0b58f7a060d47b58e4b54be8265f200e
Bruno Neckel <neckel.bw@gmail.com>
2026-08-23 00:37:05 -0300

    feat: add fornecedor create/edit form

--- src/app/features/fornecedores/fornecedores.routes.ts
+++ src/app/features/fornecedores/fornecedores.routes.ts
@@ -5,4 +5,12 @@ export const fornecedoresRoutes: Routes = [
     path: '',
     loadComponent: () => import('./lista/fornecedores-lista').then((m) => m.FornecedoresLista),
   },
+  {
+    path: 'novo',
+    loadComponent: () => import('./form/fornecedor-form').then((m) => m.FornecedorForm),
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./form/fornecedor-form').then((m) => m.FornecedorForm),
+  },
 ];
